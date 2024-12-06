1b607fb020c835e0b03a46b24960cc1e0094284a
brambleappmatus <161206934+brambleappmatus@users.noreply.github.com>
2024-12-06 01:04:16 +0100

    Delete hooks directory

--- hooks/useTranslations.ts
+++
@@ -1,6 +0,0 @@
-import { useStore } from '@/store/useStore';
-
-export const useTranslations = () => {
-  const { translations, language, setLanguage } = useStore();
-  return { translations, language, setLanguage };
-};
